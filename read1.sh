1cae21ffa543d9faa6d24dc4ea5a81457993016b
fritz-langdji <fritzarmani07@gmail.com>
2025-02-27 17:12:59 -0500

    I added read1

---
+++ read1.sh
@@ -0,0 +1,7 @@
+echo 'Please enter your name'
+read name
+echo "$name, welcome to TD Bank" 
+echo 'please enter your card' 
+echo 'please enter your pin' 
+read pin
+echo "$pin is the correct pin, $name, how may we help you today?"
